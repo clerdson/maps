ea5ebba9312b798c321ce3f5c3e0be8e686e559b
clerdson Juca <clerdson@gmail.com>
2021-04-12 20:55:47 -0300

    onmapclicklistener

--- app/src/main/java/com/example/mapsactivity/MapsActivity.java
+++ app/src/main/java/com/example/mapsactivity/MapsActivity.java
@@ -3,11 +3,13 @@ package com.example.mapsactivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +40,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //set click
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+                Toast.makeText(MapsActivity.this,
+                        "Lat: "+latitude+"Long:"+longitude,
+                        Toast.LENGTH_SHORT).show();
+                mMap.addMarker(
+                        new MarkerOptions().
+                                position(latLng).
+                                title("Local").snippet("descricao")
+                                // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_loja))
+                );
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+            }
+        });
+
+
+
+
+
     }
 }
