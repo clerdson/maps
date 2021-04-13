4646782bf6aa7318b6ace918c5f1339554866dab
clerdson Juca <clerdson@gmail.com>
2021-04-12 23:03:31 -0300

    polyline

--- app/src/main/java/com/example/mapsactivity/MapsActivity.java
+++ app/src/main/java/com/example/mapsactivity/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.mapsactivity;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -10,8 +11,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,15 +43,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //-23.5856199,-46.6669873
+        LatLng ibirapuera = new LatLng(-23.5856199, -46.6669873);
+/*
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(ibirapuera);
+        circleOptions.radius(500);
+        circleOptions.strokeColor(Color.GRAY);
+        circleOptions.strokeWidth(10);
+        circleOptions.fillColor(Color.argb(128,255,153,0));*/
 
+        //PolygonOptions polygonOptions=new PolygonOptions();
+
+
+        //mMap.addCircle(circleOptions);
+        mMap.addMarker(
+                new MarkerOptions().
+                        position(ibirapuera).
+                        title("Local").snippet("descricao")
+                        // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_carro_roxo_48px))
+        );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ibirapuera, 16));
         //set click
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
                 Double latitude = latLng.latitude;
                 Double longitude = latLng.longitude;
+
+                PolylineOptions polylineOptions = new PolylineOptions();
+                polylineOptions.add(ibirapuera);
+                polylineOptions.add(latLng);
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.width(20);
+
+                mMap.addPolyline(polylineOptions);
+
                 Toast.makeText(MapsActivity.this,
-                        "Lat: "+latitude+"Long:"+longitude,
+                        "Lat: " + latitude + "Long:" + longitude,
                         Toast.LENGTH_SHORT).show();
                 mMap.addMarker(
                         new MarkerOptions().
@@ -56,12 +90,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                 // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
                                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.icone_loja))
                 );
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+
             }
         });
-
-
-
 
 
     }
